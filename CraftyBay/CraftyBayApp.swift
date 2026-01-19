cefaef75c62fe355ab795cfb078980149a5f6791
Majid Bhuiyan <majid.bhuiyan2011@gmail.com>
2026-01-19 14:53:09 +0600

    Initial Commit

---
+++ CraftyBay/CraftyBayApp.swift
@@ -0,0 +1,17 @@
+//
+//  CraftyBayApp.swift
+//  CraftyBay
+//
+//  Created by Betopia on 19/1/26.
+//
+
+import SwiftUI
+
+@main
+struct CraftyBayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
