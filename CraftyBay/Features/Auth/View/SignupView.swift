77b0abeb1c5298dc07a00d98facbb74fcb982d8f
Majid Bhuiyan <majid.bhuiyan2011@gmail.com>
2026-02-11 12:40:02 +0600

    Test

--- CraftyBay/Features/Auth/View/SignupView.swift
+++ CraftyBay/Features/Auth/View/SignupView.swift
@@ -10,16 +10,32 @@ import SwiftUI
 struct SignupView: View {
     
     @State private var goLogin = false
+    @StateObject private var viewModel = SignupViewModel()
     var body: some View {
         NavigationStack{
-            VStack{
-               
-                    
-            }
+            VStack(){
+               Text("Create Account")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(AppColor.primaryColor)
+            Text("Sign up to get started")
+                    .foregroundColor(.gray)
+                
+                TextField("First Name", text: $viewModel.firstName)
+                    .textInputAutocapitalization(.words)
+                    .padding()
+                    .background(Color(.systemGray6))
+                    .cornerRadius(20)
+            }.padding()
+          
             
             .navigationDestination(isPresented: $goLogin){
                 LoginView()
             }
         }
     }
+}
+
+#Preview {
+    SignupView()
 }
